4d7d99df08652fc0d599f6f36c2c178d72091944
RavingKoala <stijnvb@hotmail.com>
2025-09-18 20:55:22 +0200

    changed files

--- src/main/java/com/ravingkoala/playground/PlaygroundClient.java
+++ src/main/java/com/ravingkoala/playground/PlaygroundClient.java
@@ -11,11 +11,11 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = mcmodplayground.MODID, dist = Dist.CLIENT)
+@Mod(value = Playground.MOD_ID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = mcmodplayground.MODID, value = Dist.CLIENT)
-public class mcmodplaygroundClient {
-    public mcmodplaygroundClient(ModContainer container) {
+@EventBusSubscriber(modid = Playground.MOD_ID, value = Dist.CLIENT)
+public class PlaygroundClient {
+    public PlaygroundClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
         // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
         // Do not forget to add translations for your config options to the en_us.json file.
@@ -25,7 +25,7 @@ public class mcmodplaygroundClient {
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
         // Some client setup code
-        mcmodplayground.LOGGER.info("HELLO FROM CLIENT SETUP");
-        mcmodplayground.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        Playground.LOGGER.info("HELLO FROM CLIENT SETUP");
+        Playground.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
